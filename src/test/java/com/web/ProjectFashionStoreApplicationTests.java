836e65742724c5afc6a8542bfc35212adfa4749d
nam <nthanhnam0821@gmail.com>
2026-04-01 13:44:01 +0700

    Update SpringBoot tests & Inject Firebase Secret

--- src/test/java/com/web/ProjectFashionStoreApplicationTests.java
+++ src/test/java/com/web/ProjectFashionStoreApplicationTests.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjectSellApplicationTests {
+class ProjectFashionStoreApplicationTests {
 
     @Test
     void contextLoads() {
-        // Test này sẽ kiểm tra ứng dụng Spring Boot có khởi động thành công và kết nối Database được không.
+        // Test kiểm tra Spring Boot Context có chạy được với CSDL MySQL ảo không
     }
 
 }
